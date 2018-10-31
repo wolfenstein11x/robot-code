6c6ad66971a7dc6d6bf7b1aaebd15c4fa4d131d3
alexmanning9904 <aemanni1@asu.edu>
2018-10-30 19:17:06 -0700

    Added Catkin Workspace

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/alex/Robotics/robot-code/src
+# Build directory: /home/alex/Robotics/robot-code/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/alex/Robotics/robot-code/devel:$CMAKE_PREFIX_PATH"
+export PWD="/home/alex/Robotics/robot-code/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/alex/Robotics/robot-code/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/alex/Robotics/robot-code/src:$ROS_PACKAGE_PATH"
