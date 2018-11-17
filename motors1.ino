94b616b63a01e3983f2fc728d1301533b1416580
Nat <Natalieeeeeeeeeeeeeeeeee@gmail.com>
2018-11-17 16:45:18 -0700

    Merge void setup()
    Remove unnecessary, add bracket, merge void setup()

--- motors1.ino
+++ motors1.ino
@@ -2,16 +2,15 @@
 #include <Servo.h>
 #include <ros.h>
 #define USE_STM32_HW_SERIAL 
+
 void setup() {
 int lfront = 3;
 int rfront = 4;
+Serial.begin(9600); //revise/check
+pinMode(lfront, OUTPUT);
+pinMode(rfront, OUTPUT);
 }
 
-void setup() {
-  Serial.begin(9600); //revise/check
-  pinMode(lfront, OUTPUT);
-  pinMode(rfront, OUTPUT);
-}
 
 void loop() {
   //go forward
@@ -23,13 +22,4 @@ void loop() {
   analogWrite(3, -75); //motor pin, SPEED
   analogWrite(4, -75); //motor pin, SPEED
   delay(2);
-
-}void setup() {
-  // put your setup code here, to run once:
-
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
 }
